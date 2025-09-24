6484b12c0fd4c89973d020fc8dcef58a8a954ac7
fxpineau <francois-xavier.pineau@astro.unistra.fr>
2025-09-24 08:58:10 +0200

    Set in place healpix support (sort, indexation, ...)

---
+++ src/read/hsort.rs
@@ -0,0 +1,37 @@
+use std::path::PathBuf;
+
+// ICI: FOCNTION QUi TRI ET INDEX UN FICHIER!
+
+pub fn hsort(
+  input: PathBuf,
+  iRA: usize,
+  iDec: usize,
+  output: PathBuf,
+  tmp: Option<PathBuf>,
+) -> Result<(), Box<dyn Error>> {
+  toto
+}
+
+pub fn hsort_file(
+  input: PathBuf,
+  iRA: usize,
+  iDec: usize,
+  output: PathBuf,
+  tmp: Option<PathBuf>,
+) -> Result<(), Box<dyn Error>> {
+  toto
+}
+
+pub fn hsort_files(
+  input: PathBuf,
+  iRA: usize,
+  iDec: usize,
+  output: PathBuf,
+  tmp: Option<PathBuf>,
+) -> Result<(), Box<dyn Error>> {
+  // check header compatibilty
+  // Only the first BINTABLE HDU, ...
+}
+
+// Make an iteartor from one file, and, from it...
+// Build an iterator from several files...
